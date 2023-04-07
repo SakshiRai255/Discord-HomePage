eb45d8561dc4da3cfb14e0891de348db9779bd02
Sakshi Rai <sakshirai996@gmail.com>
2023-04-07 10:40:23 +0530

    This is Discord Home page This is Successfully Created

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        brand: '#5865f2'
+      }
+    },
+  },
+  plugins: [],
+}
+
